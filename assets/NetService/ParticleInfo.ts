e2da36488f2c888c19dcab1690c26f7cb9fece0a
Zhichao Link <sunzhichao@minijoy.work>
2023-04-12 18:08:25 +0800

    update project id

--- assets/NetService/ParticleInfo.ts
+++ assets/NetService/ParticleInfo.ts
@@ -1,6 +1,6 @@
-import { ChainInfo } from "../Models/ChainInfo";
+
 
 export class ParticleInfo {
-    static projectId: string = '5479798b-26a9-4943-b848-649bb104fdc3';
-    static clientKey: string = 'cUKfeOA7rnNFCxSBtXE5byLgzIhzGrE4Y7rDdY4b';
+    static projectId: string = '77eeb8ec-dd64-48e9-b786-aa340ce42a40';
+    static clientKey: string = 'cpahKexYg891D1BQ3IwMv9fd6E5JKdizMZcFRZ6x';
 }
