f05739e56464c191b01a79d1e596460f71a47d89
Zhichao Link <sunzhichao@minijoy.work>
2023-04-13 16:56:55 +0800

    update

--- assets/provider/connection.ts
+++ assets/provider/connection.ts
@@ -1,51 +1,16 @@
-import axios from 'web3/dist/web3.min.js';;
 
+
+import { EvmService } from '../NetService/EvmService';
+import { SolanaService } from '../NetService/SolanaService';
 import type { ConnectionOptions, RequestArguments } from './types';
 
-console.log('axios ', axios);
 
-const instance : any = undefined;
-
-// const instance = axios.create({
-//     baseURL: 'https://rpc.particle.network',
-//     timeout: 30_000, // 30 secs
-// });
-
-
-function request(path: string, args: RequestArguments, config: ConnectionOptions) {
-    args.jsonrpc = '2.0';
-    if (!args.id) {
-        args.id = randomId();
-    }
-    return instance
-        .post(path, args, {
-            params: {
-                chainId: config.chainId,
-                projectUuid: config.projectId,
-                projectKey: config.clientKey,
-            },
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-        })
-        .then((res: any) => res.data);
-}
-
-export function randomId(): number {
-    const date = Date.now() * Math.pow(10, 3);
-    const extra = Math.floor(Math.random() * Math.pow(10, 3));
-    return date + extra;
-}
 
 export function sendEVMRpc(args: RequestArguments, config: ConnectionOptions) {
-    return request('/evm-chain', args, config);
+    return EvmService.rpc(args.method, args.params)
 }
 
 export function sendSolanaRpc(args: RequestArguments, config: ConnectionOptions) {
-    return request('/solana', args, config);
+    return SolanaService.rpc(args.method, args.params)
 }
 
-export function setBaseURL(baseURL: string) {
-    instance.defaults.baseURL = baseURL;
-}
--- types/particle.d.ts
+++ types/particle.d.ts
@@ -2,6 +2,3 @@ declare module "web3/dist/web3.min.js" {
     export * as default from 'web3';
 }
 
-declare module "axios/dist/axios.min.js" {
-    export * as default from 'axios';
-}
