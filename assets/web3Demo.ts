a17c95f560522081ccdd0aeb114cef11ad1b5579
Zhichao Link <sunzhichao@minijoy.work>
2023-04-13 17:06:42 +0800

    update

--- assets/web3Demo.ts
+++ assets/web3Demo.ts
@@ -5,6 +5,6 @@ import Web3 from 'web3/dist/web3.min.js';
 export const createWeb3 = () => {
     const provider = new ParticleProvider();
     // @ts-ignore
-    const web3 = new Web3(particleProvider as any | ParticleProvider);
+    const web3 = new Web3(provider);
     return web3;
 };
